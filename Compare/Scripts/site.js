51f8c447370aa0dab8f213caed4127f2157ce242
Rick Bundren <bundy845@gmail.com>
2013-04-12 14:38:43 -0400

    js not firing on load

--- Compare/Scripts/site.js
+++ Compare/Scripts/site.js
@@ -1,23 +1,26 @@
-﻿$(".taginput").focusout(function () {
+﻿
+$(function () {
+    $(".taginput").focusout(function () {
 
-    var $textBox = $(this);
-    $.post("/Home/ValidateUser", { tag: $textBox.val() }, function (data) {
-        if (data == 'False') {
-            $textBox.addClass("invalidtag");
+        var $textBox = $(this);
+        $.post("/Home/ValidateUser", { tag: $textBox.val() }, function (data) {
+            if (data == 'False') {
+                $textBox.addClass("invalidtag");
 
-        }
-        else {
-            $textBox.removeClass("invalidtag");
+            }
+            else {
+                $textBox.removeClass("invalidtag");
+            }
+        });
+    });
+
+    $("table#summaryScores").tablesorter();
+
+    $("table#detailGames").tablesorter({
+        headers: {
+            0: {
+                sorter: false
+            }
         }
     });
-});
-
-$("table#summaryScores").tablesorter();
-
-$("table#detailGames").tablesorter({
-    headers: {
-        0: {
-            sorter: false
-        }
-    }
 });
